5c63a38639d6b2200fc8c4bbb4523bbca73fcae2
JTrochez-1 <kokouchiha1@gmail.com>
2019-11-19 07:58:59 -0600

    Se agrego una linea de codigo

---
+++ CentroSalud/CentroSalud/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CentroSalud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba Git");
+        }
+    }
+}
